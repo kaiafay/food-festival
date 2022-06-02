ec9fe614d64cb3ea7aabb5636a8e9453e9a5995c
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-01 18:12:51 -0700

    install image webpack loader and optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = {
                           return url.replace("../", "/assets/")
                         }
                       }
+                },
+                {
+                    loader: 'image-webpack-loader'
                 }
             ]
           }
