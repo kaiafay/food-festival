cb9d2372c6d59c166f25a6cda48325229b661fb1
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-01 17:54:15 -0700

    add multiple entry points to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,16 @@ const path = require('path');
 // main configuration object that tells webpack what to do
 module.exports = {
     // root of the bundle and beginning of the dependency graph
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     // output bundled code to specified folder
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
     },
     plugins: [
         new webpack.ProvidePlugin({
